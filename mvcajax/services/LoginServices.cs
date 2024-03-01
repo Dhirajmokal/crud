f2cd58b9ed29b0029877a89f1a25c99890147905
dhira <dhira@DM_PC>
2024-03-01 17:37:14 +0530

    Add project files.

---
+++ mvcajax/services/LoginServices.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using mvcajax.Models;
+
+
+namespace mvcajax.services
+{
+    public class LoginServices
+    {
+        SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.AppSettings["connection"]);
+        public void postuser(Models.Login namedata) 
+        { 
+
+            SqlCommand cmd = new SqlCommand("postuser", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Username", namedata.Username);
+           
+            cmd.Parameters.AddWithValue("@pass", namedata.Password);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+        public void putuser(Models.Login namedata)
+        {
+            SqlCommand cmd = new SqlCommand("putuser", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", namedata.Id);
+            cmd.Parameters.AddWithValue("@Username", namedata.Username);
+          
+            cmd.Parameters.AddWithValue("@pass", namedata.Password);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+        public void deleteuser(Models.Login namedata)
+        {
+            SqlCommand cmd = new SqlCommand("deleteuser", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+          
+            cmd.Parameters.AddWithValue("@Id", namedata.Id);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+    }
+}
